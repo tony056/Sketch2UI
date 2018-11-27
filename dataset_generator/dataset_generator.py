c31d6e2c5fa70ef1bdaea42d603839e107cf001e
khzeng <jacky55062003@gmail.com>
2018-11-26 23:53:57 -0800

    add anatation generator & debug

--- dataset_generator/dataset_generator.py
+++ dataset_generator/dataset_generator.py
@@ -1,9 +1,11 @@
 # execution: python3 dataset_analysis.py --ps [PATH_TO_Rico_DATASET_DIR] --pc [PATH_TO_CJ_DATASET_DIR] 
 #                                        --po [PATH_TO_OUTPUT_DATASET_DIR] --pm [PATH_TO_MAP_FILE]
 #                                        --gm [INDICATE_TO_GENERATE_MAP] --pcjc [PATH_TO_CJ_FILES_COMPONENTS_JSON]
+#                                        --pa [PATH_TO_OUTPUT_ANATATIONS]
 import json
 import sys
 import os
+import fnmatch
 import collections
 import argparse
 import pdb
@@ -12,6 +14,7 @@ import numpy as np
 from PIL import Image
 
 # result:
+#     annotations.json
 #     0.jpg,
 #     1.jpg,
 #       ...
@@ -39,6 +42,9 @@ def parse_args():
     parser.add_argument('--pcjc', dest='pcjc',
                         help='file path to CJ_files_components.json',
                         default='CJ_files_components.json', type=str)
+    parser.add_argument('--pa', dest='pa',
+                        help='file path to annotations.json',
+                        default='annotations.json', type=str)
 
     args = parser.parse_args()
     return args
@@ -86,6 +92,7 @@ def parse_json_files(args, Map):
             components = [(components[ii * 2], components[ii * 2 + 1]) for ii in range(len(components) // 2)]
             Rico_label = []
             CJ_label = []
+            CJ_class = []
             bounds = []
             for component in components:
                 label = component[0]
@@ -93,14 +100,16 @@ def parse_json_files(args, Map):
                 try:
                     Rico_label.append(label)
                     bounds.append(com_bounds)
-                    CJ_label.append(Map['Rico2CJ'][label])
+                    CJ_label.append(Map['Rico2CJ'][label].keys()[0])
+                    CJ_class.append(Map['Rico2CJ'][label].values()[0])
                 except KeyError:
                     CJ_label.append('')
+                    CJ_class.append('')
                     pb_triger = True
             if pb_triger:
-                Pb_files_components[filename.split('.')[0]] = {'Rico_label': Rico_label, 'CJ_label': CJ_label, 'bounds': bounds}
+                Pb_files_components[filename.split('.')[0]] = {'Rico_label': Rico_label, 'CJ_label': CJ_label, 'CJ_class': CJ_class, 'bounds': bounds}
             else:
-                CJ_files_components[filename.split('.')[0]] = {'Rico_label': Rico_label, 'CJ_label': CJ_label, 'bounds': bounds}
+                CJ_files_components[filename.split('.')[0]] = {'Rico_label': Rico_label, 'CJ_label': CJ_label, 'CJ_class': CJ_class, 'bounds': bounds}
 
     if not os.path.isfile(args.pcjc):
         with open(args.pcjc, 'w') as output_file:
@@ -113,26 +122,37 @@ def parse_json_files(args, Map):
 def generate_data(args, CJ_files_components):
     if not os.path.isdir(args.po):
         os.mkdir(args.po)
+    components_label = collections.defaultdict(list)
+    components_label['annotations'] = []
+    cnt = 0
     for filename, value in CJ_files_components.items():
         components = value['CJ_label']
+        classes = value['CJ_class']
         positions = value['bounds']
         output = np.array([[[1.]*3]*1440]*2560)
         for ii, component in enumerate(components):
-            candidates = os.listdir(args.pc + '/' + component + '/')
+            candidates = fnmatch.filter(os.listdir(args.pc + '/' + component + '/'), '*.jpg')
             position = positions[ii]
-            try:
-                comp_img = Image.open(args.pc + '/' + component + '/' + random.choice(candidates))
-            except:
+            comp_img = Image.open(args.pc + '/' + component + '/' + random.choice(candidates))
+            if (position[2]-1 - position[0]) * (position[3]-1 - position[1]) < 25:
+                # ignore the component that component's area less than 25
                 continue
             comp_img = np.array(comp_img.resize((position[2]-1 - position[0], position[3]-1 - position[1]), Image.ANTIALIAS))
             comp_img = comp_img / 255.
             output[position[1]:position[3]-1, position[0]:position[2]-1, :] *= comp_img
+            components_label['annotations'].append({'bbox': [position[0], position[1], position[2]-1-position[0], position[3]-1-position[1]], 'image_id': int(filename), 'category_id': classes[ii], 'id': cnt})
+            cnt += 1
         output = Image.fromarray(np.uint8(output * 255.))
         output.save(args.po + '/' + filename + '.jpg')
+    with open(args.pa, 'w') as output_file:
+        json.dump(components_label, output_file, sort_keys=True, indent=4, separators=(',', ': '))
 
 def generate_map(MAP_DIR):
-    CJ2Rico = {'Images': ['Background Image', 'Image', 'Web View', 'Video', 'Map View'], 'Text': ['Text'], 'Radio_Button': ['Radio Button', 'On/Off Switch'], 'Input': ['Input'], 'CheckBox': ['Checkbox'], 'Slider': ['Slider'], 'Button': ['Text Button'], 'Icon': ['Icon'], 'Pager_Indicator': ['Pager Indicator']}
-    Rico2CJ = {val: key for key, vals in CJ2Rico.items() for val in vals}
+    CJ2Rico = {3: {'Images': ['Background Image', 'Image', 'Web View', 'Video', 'Map View']},\
+               8: {'Text': ['Text']}, 6: {'Radio_Button': ['Radio Button', 'On/Off Switch']},\
+               4: {'Input': ['Input']}, 1: {'CheckBox': ['Checkbox']}, 7: {'Slider': ['Slider']},\
+               0: {'Button': ['Text Button']}, 2: {'Icon': ['Icon']}, 5: {'Pager_Indicator': ['Pager Indicator']}}
+    Rico2CJ = {Rico_label: {CJ_label: label} for label, CJ in CJ2Rico.items() for CJ_label, Rico_labels in CJ.items() for Rico_label in Rico_labels}
     Map = {'CJ2Rico': CJ2Rico, 'Rico2CJ': Rico2CJ}
     with open(MAP_DIR, 'w') as output_file:
         json.dump(Map, output_file, sort_keys=True, indent=4, separators=(',', ': '))
